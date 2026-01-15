17a15eaa83115bdcaba737c22b9488c2845d90b8
SzamanHerbaty <kuba.pietrasik1234@gmail.com>
2026-01-15 19:24:16 +0100

    Eltromax

---
+++ CSS/Eltromax/script.js
@@ -0,0 +1,29 @@
+let menu_btn = document.querySelector('.menu_btn');
+let nav_links = document.querySelector('.mobile_nav');
+
+menu_btn.addEventListener('click', () => {
+    const isOpen = nav_links.classList.contains('open');
+    if (isOpen) {
+        nav_links.classList.remove('open');
+        menu_btn.setAttribute('aria-expanded', 'false');
+        return;
+    } else {
+        nav_links.classList.add('open');
+        menu_btn.setAttribute('aria-expanded', 'true');
+        return;
+    }
+});
+
+// Ukryj mobile_nav automatycznie gdy szerokość okna > 1150px
+function checkWidthAndHide() {
+    if (window.innerWidth > 1150) {
+        if (nav_links.classList.contains('open')) {
+            nav_links.classList.remove('open');
+            menu_btn.setAttribute('aria-expanded', 'false');
+        }
+    }
+}
+
+window.addEventListener('resize', checkWidthAndHide);
+
+
